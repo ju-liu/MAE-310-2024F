d0b0180c6264c460c28dbb5f1ea8f2f4db741cbc
Ju Liu <liujuy@gmail.com>
2024-11-18 18:08:47 +0800

    I have finished my FE driver code and it has been verified using the manufactured solution

--- codes/proj-01-heat1d/driver.m
+++ codes/proj-01-heat1d/driver.m
@@ -1,17 +1,17 @@
 clear all; clc; % clean the memory and screen
 
 % Define the external source or force and boundary data
-f = @(x) x; % f(x) = x
-g = 1.0;    % u    = g  at x = 1
-h = 0.5;    % -u,x = h  at x = 0
+f = @(x) -20*x.^3; % f(x) = x
+g = 1.0;           % u    = g  at x = 1
+h = 0.0;           % -u,x = h  at x = 0
 
 % Setup the mesh
-pp   = 1;              % polynomial degree
+pp   = 2;              % polynomial degree
 n_en = pp + 1;         % number of element or local nodes
 n_el = 4;              % number of elements
 n_np = n_el * pp + 1;  % number of nodal points
 n_eq = n_np - 1;       % number of equations
-n_int = pp;
+n_int = 10;
 
 hh = 1.0 / (n_np - 1); % space between two adjacent nodes
 x_coor = 0 : hh : 1;   % nodal coordinates for equally spaced nodes
@@ -33,49 +33,59 @@ ID(end) = 0;
 
 % allocate the stiffness matrix
 K = zeros(n_eq, n_eq);
+F = zeros(n_eq, 1);
 
 for ee = 1 : n_el
   
   k_ele = zeros(n_en, n_en); % allocate a zero element stiffness matrix
-  
-  %x_ele = zeros(n_en, 1);
-  %for aa = 1 : n_en
-  %  x_ele(aa) = x_coor(IEN(ee,aa));
-  %end
+  f_ele = zeros(n_en, 1);    % allocate a zero element load vector
+
   x_ele = x_coor(IEN(ee,:));
 
   for qua = 1 : n_int
     
     dx_dxi = 0.0;
+    x_l = 0.0;
     for aa = 1 : n_en
+      x_l = x_l + x_ele(aa) * PolyShape(pp, aa, xi(qua), 0);
       dx_dxi = dx_dxi + x_ele(aa) * PolyShape(pp, aa, xi(qua), 1);
     end
     dxi_dx = 1.0 / dx_dxi;
 
     for aa = 1 : n_en
+      f_ele(aa) = f_ele(aa) + weight(qua) * PolyShape(pp, aa, xi(qua), 0) * f(x_l) * dx_dxi;
       for bb = 1 : n_en
         k_ele(aa, bb) = k_ele(aa, bb) + weight(qua) * PolyShape(pp, aa, xi(qua), 1) * PolyShape(pp, bb, xi(qua), 1) * dxi_dx;
       end
     end
   end
-  
+ 
   % check the ID(IEN(ee, aa)) and ID(IEN(ee,bb)), if they are positive
   % put the element stiffness matrix into K
   for aa = 1 : n_en
     P = ID(IEN(ee,aa));
     if(P > 0)
+      F(P) = F(P) + f_ele(aa);
       for bb = 1 : n_en
         Q = ID(IEN(ee,bb));
         if(Q > 0)
           K(P, Q) = K(P, Q) + k_ele(aa, bb);
+        else
+          F(P) = F(P) - k_ele(aa, bb) * g; % handles the Dirichlet boundary data
         end
       end
     end
   end
 
+  if ee == 1
+    F(ID(IEN(ee,1))) = F(ID(IEN(ee,1))) + h;
+  end
 end
 
+% Solve Kd = F equation
+d_temp = K \ F;
 
+disp = [d_temp; g];
 
 
 
