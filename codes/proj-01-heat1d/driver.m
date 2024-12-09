8d80a4611d6c90b0391dd6de635da2a7e7e453aa
Ju Liu <liujuy@gmail.com>
2024-12-09 17:49:24 +0800

    error calculator added

--- codes/proj-01-heat1d/driver.m
+++ codes/proj-01-heat1d/driver.m
@@ -5,10 +5,13 @@ f = @(x) -20*x.^3; % f(x) is the source
 g = 1.0;           % u    = g  at x = 1
 h = 0.0;           % -u,x = h  at x = 0
 
+exact = @(x) x.^5;
+exact_x = @(x) 5 * x.^4;
+
 % Setup the mesh
 pp   = 2;              % polynomial degree
 n_en = pp + 1;         % number of element or local nodes
-n_el = 2;              % number of elements
+n_el = 16;              % number of elements
 n_np = n_el * pp + 1;  % number of nodal points
 n_eq = n_np - 1;       % number of equations
 n_int = 10;
@@ -130,35 +133,39 @@ hold on;
 plot(x_sam, y_sam, '-k','LineWidth',3);
 
 
+% calculate the error
+nqp = 10;
+[xi, weight] = Gauss(nqp, -1, 1);
 
+L2_top = 0.0; L2_bot = 0.0; H1_top = 0.0; H1_bot = 0.0;
 
+for ee = 1 : n_el
+  x_ele = x_coor( IEN(ee, :) );
+  u_ele = disp( IEN(ee, :) );
 
+  for ll = 1 : nqp
+    x_l = 0.0; uh = 0.0; dx_dxi = 0.0; uh_xi = 0.0;
+    for aa = 1 : n_en
+      x_l    = x_l    + x_ele(aa) * PolyShape(pp, aa, xi(ll), 0);
+      uh     = uh     + u_ele(aa) * PolyShape(pp, aa, xi(ll), 0);
+      dx_dxi = dx_dxi + x_ele(aa) * PolyShape(pp, aa, xi(ll), 1);
+      uh_xi  = uh_xi  + u_ele(aa) * PolyShape(pp, aa, xi(ll), 1);
+    end
+    dxi_dx = 1.0 / dx_dxi;
 
+    L2_top = L2_top + weight(ll) * (uh - exact(x_l))^2 * dx_dxi;
+    L2_bot = L2_bot + weight(ll) * exact(x_l)^2 * dx_dxi;
 
+    H1_top = H1_top + weight(ll) * ( uh_xi * dxi_dx - exact_x(x_l) )^2 * dx_dxi;
+    H1_bot = H1_bot + weight(ll) * exact_x(x_l)^2 * dx_dxi;
 
+  end
+end
 
+L2_top = sqrt(L2_top); L2_bot = sqrt(L2_bot);
+H1_top = sqrt(H1_top); H1_bot = sqrt(H1_bot);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+L2_error = L2_top / L2_bot;
+H1_error = H1_top / H1_bot;
 
 % EOF
