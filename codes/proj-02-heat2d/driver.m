a26cb191024ed1f17950dadc7d352578b1f8df41
Ju Liu <liujuy@gmail.com>
2024-12-09 18:16:10 +0800

    in

--- codes/proj-02-heat2d/driver.m
+++ codes/proj-02-heat2d/driver.m
@@ -1,9 +1,85 @@
 clear all; clc;
 
-kappa = 1.0 % conductivity
+kappa = 1.0; % conductivity
 
 exact = @(x,y) x*(1-x)*y*(1-y); % exact solution
 exact_x = @(x,y) (1-2*x)*y*(1-y);
 exact_y = @(x,y) x*(1-x)*(1-2*y);
 
 f = @(x,y) 2*kappa*x*(1-x)+2*kappa*y*(1-y); % source
+
+% mesh generation
+n_en = 4;
+n_el_x = 5; % number of elements in x-dir
+n_el_y = 6; % number of elements in y-dir
+n_el   = n_el_x * n_el_y; % total number of elements
+
+n_np_x = n_el_x + 1; % number of nodal points in x-dir
+n_np_y = n_el_y + 1; % number of nodal points in y-dir
+n_np   = n_np_x * n_np_y; % total number of nodal points
+
+x_coor = zeros(n_np, 1);
+y_coor = x_coor;
+
+hx = 1.0 / n_el_x; % mesh size in x-dir
+hy = 1.0 / n_el_y; % mesh size in y-dir
+
+for ny = 1 : n_np_y
+  for nx = 1 : n_np_x
+    index = (ny-1)*n_np_x + nx;
+    x_coor(index) = (nx-1) * hx;
+    y_coor(index) = (ny-1) * hy;
+  end
+end
+
+% IEN array
+IEN = zeros(n_el, n_en);
+for ex = 1 : n_el_x
+  for ey = 1 : n_el_y
+    ee = (ey-1) * n_el_x + ex;
+    IEN(ee, 1) = (ey-1)*n_np_x + ex;
+    IEN(ee, 2) = (ey-1)*n_np_x + ex + 1;
+    IEN(ee, 3) = ey * n_np_x + ex + 1;
+    IEN(ee, 4) = ey * n_np_x + ex;
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
