dd08ef1fd7d7079a13bb883f9f8c635ddfeaae9f
Ju Liu <liujuy@gmail.com>
2024-12-15 22:33:14 +0800

    in

--- codes/proj-02-heat2d/driver.m
+++ codes/proj-02-heat2d/driver.m
@@ -2,31 +2,33 @@ clear all; clc;
 
 kappa = 1.0; % conductivity
 
-exact = @(x,y) x*(1-x)*y*(1-y); % exact solution
+% exact solution
+exact = @(x,y) x*(1-x)*y*(1-y);
 exact_x = @(x,y) (1-2*x)*y*(1-y);
 exact_y = @(x,y) x*(1-x)*(1-2*y);
 
-f = @(x,y) 2*kappa*x*(1-x)+2*kappa*y*(1-y); % source
+f = @(x,y) 2*kappa*x*(1-x)+2*kappa*y*(1-y); % source term
 
 % mesh generation
-n_en = 4;
-n_el_x = 5; % number of elements in x-dir
-n_el_y = 6; % number of elements in y-dir
+n_en   = 4;               % number of nodes in an element
+n_el_x = 5;               % number of elements in x-dir
+n_el_y = 6;               % number of elements in y-dir
 n_el   = n_el_x * n_el_y; % total number of elements
 
-n_np_x = n_el_x + 1; % number of nodal points in x-dir
-n_np_y = n_el_y + 1; % number of nodal points in y-dir
+n_np_x = n_el_x + 1;      % number of nodal points in x-dir
+n_np_y = n_el_y + 1;      % number of nodal points in y-dir
 n_np   = n_np_x * n_np_y; % total number of nodal points
 
 x_coor = zeros(n_np, 1);
 y_coor = x_coor;
 
-hx = 1.0 / n_el_x; % mesh size in x-dir
-hy = 1.0 / n_el_y; % mesh size in y-dir
+hx = 1.0 / n_el_x;        % mesh size in x-dir
+hy = 1.0 / n_el_y;        % mesh size in y-dir
 
+% generate the nodal coordinates
 for ny = 1 : n_np_y
   for nx = 1 : n_np_x
-    index = (ny-1)*n_np_x + nx;
+    index = (ny-1)*n_np_x + nx; % nodal index
     x_coor(index) = (nx-1) * hx;
     y_coor(index) = (ny-1) * hy;
   end
@@ -36,11 +38,11 @@ end
 IEN = zeros(n_el, n_en);
 for ex = 1 : n_el_x
   for ey = 1 : n_el_y
-    ee = (ey-1) * n_el_x + ex;
-    IEN(ee, 1) = (ey-1)*n_np_x + ex;
-    IEN(ee, 2) = (ey-1)*n_np_x + ex + 1;
-    IEN(ee, 3) = ey * n_np_x + ex + 1;
-    IEN(ee, 4) = ey * n_np_x + ex;
+    ee = (ey-1) * n_el_x + ex; % element index
+    IEN(ee, 1) = (ey-1) * n_np_x + ex;
+    IEN(ee, 2) = (ey-1) * n_np_x + ex + 1;
+    IEN(ee, 3) =  ey    * n_np_x + ex + 1;
+    IEN(ee, 4) =  ey    * n_np_x + ex;
   end
 end
 
@@ -48,38 +50,4 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% EOF
---
+++ codes/proj-02-heat2d/vis_2d_sol.m
@@ -0,0 +1,21 @@
+clear all; clc; clf;
+
+load("HEAT.mat");
+
+hh_x = 1.0 / n_el_x;
+hh_y = 1.0 / n_el_y;
+
+n_np_x = n_el_x + 1;
+n_np_y = n_el_y + 1;
+
+[X, Y] = meshgrid(0 : hh_x : 1, 0 : hh_y : 1);
+Z = reshape(disp, n_np_x, n_np_y);
+surf(X, Y, Z);
+
+shading interp
+
+az = -61;
+el = 20;
+view(az,el);
+
+% EOF
